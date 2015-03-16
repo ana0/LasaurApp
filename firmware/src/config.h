e9775e6a3f6266f8255914fa42a708ae0d057207
LasersaurTTL <roryrennie@gmail.com>
2015-03-16 20:38:13 +0000

    initial commit

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -29,19 +29,19 @@
 // build for new driveboard hardware
 #define DRIVEBOARD
 
-//#define V1401
+#define V1401
 
 #define BAUD_RATE 57600
 // #define DEBUG_IGNORE_SENSORS  // set for debugging
 
 
 #ifndef V1401
-  #define CONFIG_X_STEPS_PER_MM 33.26580370 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
-  #define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_X_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_Y_STEPS_PER_MM 89.47745168 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
   #define CONFIG_Z_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 #else
-  #define CONFIG_X_STEPS_PER_MM 33.26580370 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
-  #define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_X_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_Y_STEPS_PER_MM 89.47745168 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
   #define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 #endif
 #define CONFIG_PULSE_MICROSECONDS 5
@@ -50,7 +50,7 @@
 #define CONFIG_ACCELERATION 1800000.0 // mm/min^2, typically 1000000-8000000, divide by (60*60) to get mm/sec^2
 #define CONFIG_JUNCTION_DEVIATION 0.006 // mm
 #define CONFIG_X_ORIGIN_OFFSET 5.0  // mm, x-offset of table origin from physical home
-#define CONFIG_Y_ORIGIN_OFFSET 5.0  // mm, y-offset of table origin from physical home
+#define CONFIG_Y_ORIGIN_OFFSET 10.0  // mm, y-offset of table origin from physical home
 #define CONFIG_Z_ORIGIN_OFFSET 0.0   // mm, z-offset of table origin from physical home
 #ifndef V1401
   #define CONFIG_INVERT_X_AXIS 0  // 0 is regular, 1 inverts the y direction
